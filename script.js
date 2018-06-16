def8e0987432c229e643209428ca3f27ee8d78dd
Eris Allkanjari <eris.allkanjari@gmail.com>
2018-06-16 21:40:26 +0200

    Added JavaScript to a new file

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+    window.addEventListener('load', init, false);
+    function init() {
+      document.querySelectorAll('textarea').forEach((element) => {
+        element.addEventListener('keydown', autoTextareaGrow, false);
+        element.addEventListener('keyup', autoTextareaGrow, false);
+      });
+    }
+    function autoTextareaGrow() {
+      this.style.height = ''; // Reset height on each call.
+      this.style.height = 'calc(' + this.scrollHeight + 'px - 1em)'; // -1em offset of scrollHeight (because of 1em total vertical padding).
+      this.setAttribute('value', this.value.trim() ? this.value : '');
+    }
+  </script>
